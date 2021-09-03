fbd8a977db8471788fcaa125e69522b03e667490
Diego <aquinodiegoexequiel@gmail.com>
2021-09-03 19:03:46 -0300

    Se arreglan los Comparators

--- src/tpTurismoEnLaTierraMedia/TiempoComparator.java
+++ src/tpTurismoEnLaTierraMedia/TiempoComparator.java
@@ -1,5 +1,12 @@
 package tpTurismoEnLaTierraMedia;
 
-public class TiempoComparator {
+import java.util.Comparator;
+
+public class TiempoComparator implements Comparator<Atraccion>{
+	
+	@Override
+	public int compare(Atraccion a1, Atraccion a2) {
+		return Double.compare(a1.getPromedioDeTiempo(), a2.getPromedioDeTiempo());
+	}
 
 }
--- src/tpTurismoEnLaTierraMedia/TipoComparator.java
+++ src/tpTurismoEnLaTierraMedia/TipoComparator.java
@@ -1,5 +1,10 @@
 package tpTurismoEnLaTierraMedia;
+import java.util.Comparator;
 
-public class TipoComparator {
-
+public class TipoComparator implements Comparator<Atraccion>{
+	
+	@Override
+	public int compare(Atraccion o1, Atraccion o2) {
+		return o1.getTipoAtraccion().compareTo(o2.getTipoAtraccion());
+	}
 }
